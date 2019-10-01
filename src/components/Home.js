b44db95b291a1bc30852bc981d48efacd240effa
github username <github email address>
2019-10-01 10:58:20 -0400

    working on touching up the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Button, Form, Grid, Header, Image, Message, Segment, Container, Icon } from 'semantic-ui-react'
+import { connect } from 'react-redux'
 
 class Home extends Component {
 
@@ -10,18 +11,19 @@ class Home extends Component {
               <Grid.Row>
                 <Grid.Column width={8}>
                   <Header as='h3' style={{ fontSize: '2em' }}>
-                    OMG is this working? It is? Yay! ^_^
+                    Welcome to Flipout! ^_^
                   </Header>
                   <p style={{ fontSize: '1.33em' }}>
-                      Oh yeah, time to get this baby started! Time to revamp my application using some good 'ol semantic.
+                      The pokemon are hiding and will only leave with their partners which you have to find as quickly as possible! Do you have what it takes to flip them all?
                   </p>
-                  <Header as='h3' style={{ fontSize: '2em' }}>
+                  {/* <Header as='h3' style={{ fontSize: '2em' }}>
                     I Make Bananas That Can Dance
                   </Header>
                   <p style={{ fontSize: '1.33em' }}>
                     Yes that's right, you thought it was the stuff of dreams, but even bananas can be
                     bioengineered.
-                  </p>
+                  </p> */}
+                  <br></br>
                 </Grid.Column>
                 <Grid.Column floated='right' width={6}>
                   <Image bordered rounded size='huge' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1vpx6KtbjypNFvMbzccJBP6cWHONT-c9bYTujD0UF1IrtpK6r' />
@@ -29,7 +31,7 @@ class Home extends Component {
               </Grid.Row>
               <Grid.Row>
                 <Grid.Column textAlign='center'>
-                  <Button size='huge'>Check This Out!   <Icon name="smile" /></Button>
+                  <Button size='huge'>Click here to start flipping! <Icon name="smile" /></Button>
                 </Grid.Column>
               </Grid.Row>
             </Grid>
@@ -38,4 +40,5 @@ class Home extends Component {
     }
 }
 
-export default Home
+
+export default connect()(Home)
