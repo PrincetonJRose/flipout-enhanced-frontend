24312ada9e841dea25ffad88fca9c37c8f77f20d
PrincetonJRose <decoyzpr@gmail.com>
2020-04-29 11:34:30 -0400

    pushing to start working on it again

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -1,36 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Container, Image, Card } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import ReactCardFlip from 'react-card-flip'
 
-class FlipCard extends Component {
-  constructor() {
-    super()
-  }
+const FlipCard = props => {
 
-  handleClick =(e)=> {
-    e.preventDefault()
-    if (!this.props.card.isFlipped && this.props.compare.length < 2) {
-      this.props.dispatch({ type: 'FLIP_CARD', card: this.props.card })
+  function handleClick() {
+    if (!props.card.isFlipped && props.compare.length < 2) {
+      props.dispatch({ type: 'FLIP_CARD', card: props.card })
     }
   }
 
-  render () {
-    return (
-      <Container align='center' centered >
-        <div className="flip-card" align='center'>
-          <ReactCardFlip isFlipped={this.props.card.isFlipped} flipDirection="horizontal">
-            <div className="flip-card-front" onClick={this.handleClick} key="front">
-              <Image centered src={this.props.cardBack} size='small' />
-            </div>
-            <div className="flip-card-back" onClick={this.handleClick} key="back">
-              <Image centered src={this.props.card.image_url} wrapped />
-            </div>
-          </ReactCardFlip>
-        </div>
-      </Container>
-    )
-  }
+  return (
+    <Container align='center' centered >
+      <div className="flip-card" align='center'>
+        <ReactCardFlip isFlipped={props.card.isFlipped} flipDirection="horizontal">
+          <div className="flip-card-front" onClick={handleClick} key="front">
+            <Image centered src={props.cardBack} size='small' />
+          </div>
+          <div className="flip-card-back" onClick={handleClick} key="back">
+            <Image centered src={props.card.image_url} wrapped />
+          </div>
+        </ReactCardFlip>
+      </div>
+    </Container>
+  )
 }
 
 let mapStateToProps =(state)=> {
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,88 +1,86 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { Link, Redirect } from 'react-router-dom'
 import { Button, Form, Grid, Header, Image, Message, Segment, Container, Icon, Statistic, Modal, Transition } from 'semantic-ui-react'
 import FlipCard from '../components/FlipCard'
 
-class MainContainer extends Component {
+const MainContainer = props => {
 
-  checkGameProgress =()=> {
+  function checkGameProgress () {
 
   }
 
-  flipMatchedCards =()=> {
-    this.props.dispatch({ type: 'FLIP_MATCHED_CARDS' })
+  function flipMatchedCards () {
+    props.dispatch({ type: 'FLIP_MATCHED_CARDS' })
   }
 
-    render () {
-      let stats = this.props.gameStats
-      if (this.props.compare.length >= 2)
-        setTimeout( this.flipMatchedCards, 1100)
-      if (this.props.gameDeck.length > 0) {
-        return (
-          <Grid container verticalAlign='middle' columns={this.props.columns}>
-            <Modal centered id='winGameModal' open={stats.win} closeIcon onClose={ ()=> {
-              this.props.dispatch({ type: 'RESET_GAME_STATS' })
-            } }  >
-              <Modal.Header centered ><Icon floated='left' name='trophy' /> You win!!! <Icon name='trophy' /></Modal.Header>
-              <Modal.Content image>
-                <Image wrapped size='small' src='https://cdn.bulbagarden.net/upload/a/a7/PSMD_poster.png' />
-                <Modal.Description>
-                  <Header>Good job!</Header>
-                  <p>
-                    You were able to locate all of the pokemon and their partners! You should feel proud! Now its time to work on improving your skills for future pokemon who may need your assistance! <Icon name='smile' />
-                  </p>
-                </Modal.Description>
-              </Modal.Content>
-            </Modal>
-            <Grid.Row centered >
-              <br></br>
-              <Header as='h3' centered><u>Current Game Stats</u>:</Header>
-            </Grid.Row>
-            <Grid.Row centered >
-              <Statistic.Group size='mini'>
-                <Statistic>
-                  <Statistic.Label>Current Combo</Statistic.Label>
-                  <Statistic.Value>{stats.combo}</Statistic.Value>
-                </Statistic>
-                <Statistic>
-                  <Statistic.Label>Highest Combo</Statistic.Label>
-                  <Statistic.Value>{stats.comboChain}</Statistic.Value>
-                </Statistic>
-                <Statistic>
-                  <Statistic.Label>Turns Taken</Statistic.Label>
-                  <Statistic.Value>{stats.turns}</Statistic.Value>
-                </Statistic>
-                <Statistic>
-                  <Statistic.Label>Misses</Statistic.Label>
-                  <Statistic.Value>{stats.misses}</Statistic.Value>
-                </Statistic>
-              </Statistic.Group>
-            </Grid.Row>
-            <br></br>
-          {
-            this.props.gameDeck.map(card => (
-              <Grid.Column>
-                <FlipCard card={card} compare={this.props.compare}/>
-              </Grid.Column>
-            ))
-          }
-        </Grid>
-        )
-      } else {
-        return (
-          <Container centered >
-            <br></br>
-            <Segment centered raised>
-              <Header align='center'>
-                  <Icon className='up-arrow' name='arrow alternate circle up outline' />
-                Click on the New Game button to start!
-              </Header>
-            </Segment>
-          </Container>
-        )
+  let stats = props.gameStats
+  if (props.compare.length >= 2)
+    setTimeout( flipMatchedCards, 1100)
+  if (props.gameDeck.length > 0) {
+    return (
+      <Grid container verticalAlign='middle' columns={props.columns}>
+        <Modal centered id='winGameModal' open={stats.win} closeIcon onClose={ ()=> {
+          props.dispatch({ type: 'RESET_GAME_STATS' })
+        } }  >
+          <Modal.Header centered ><Icon floated='left' name='trophy' /> You win!!! <Icon name='trophy' /></Modal.Header>
+          <Modal.Content image>
+            <Image wrapped size='small' src='https://cdn.bulbagarden.net/upload/a/a7/PSMD_poster.png' />
+            <Modal.Description>
+              <Header>Good job!</Header>
+              <p>
+                You were able to locate all of the pokemon and their partners! You should feel proud! Now its time to work on improving your skills for future pokemon who may need your assistance! <Icon name='smile' />
+              </p>
+            </Modal.Description>
+          </Modal.Content>
+        </Modal>
+        <Grid.Row centered >
+          <br></br>
+          <Header as='h3' centered><u>Current Game Stats</u>:</Header>
+        </Grid.Row>
+        <Grid.Row centered >
+          <Statistic.Group size='mini'>
+            <Statistic>
+              <Statistic.Label>Current Combo</Statistic.Label>
+              <Statistic.Value>{stats.combo}</Statistic.Value>
+            </Statistic>
+            <Statistic>
+              <Statistic.Label>Highest Combo</Statistic.Label>
+              <Statistic.Value>{stats.comboChain}</Statistic.Value>
+            </Statistic>
+            <Statistic>
+              <Statistic.Label>Turns Taken</Statistic.Label>
+              <Statistic.Value>{stats.turns}</Statistic.Value>
+            </Statistic>
+            <Statistic>
+              <Statistic.Label>Misses</Statistic.Label>
+              <Statistic.Value>{stats.misses}</Statistic.Value>
+            </Statistic>
+          </Statistic.Group>
+        </Grid.Row>
+        <br></br>
+      {
+        props.gameDeck.map(card => (
+          <Grid.Column>
+            <FlipCard card={card} compare={props.compare}/>
+          </Grid.Column>
+        ))
       }
-    }
+    </Grid>
+    )
+  } else {
+    return (
+      <Container centered >
+        <br></br>
+        <Segment centered raised>
+          <Header centered >
+              <Icon className='up-arrow' name='arrow alternate circle up outline' />
+            Click on the New Game button to start!
+          </Header>
+        </Segment>
+      </Container>
+    )
+  }
 }
 
 let mapStateToProps =(state)=> {
